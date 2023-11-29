d919223bb6dd3878fffcc1e723bc5f146472a07b
javiergarciiiaa <fcojaviergarciamartinez@outlook.es>
2023-11-29 19:36:51 +0100

    Ejercicio 7

--- Proyecto3_3/src/Clase3_3.java
+++ Proyecto3_3/src/Clase3_3.java
@@ -3,7 +3,7 @@ public class Clase3_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Practica 3_3 Francisco Javier García");
+		System.out.println("Canela En Rama");
 	}
 
 }
